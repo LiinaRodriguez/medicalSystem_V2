de0e76d98642ad2c29b457890bc25953f8dee52e
liinarodriguez <lsrodriguez@unillanos.edu.co>
2023-03-07 10:22:09 -0500

    seeders fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,8 @@ class DatabaseSeeder extends Seeder{
         // ]);
 
         $this->call(UsersTableSeeder::class);
+        $this->call(DepartmentsTableSeeder::class);
+        $this->call(CitiesTableSeeder::class);
+
     }
 }
